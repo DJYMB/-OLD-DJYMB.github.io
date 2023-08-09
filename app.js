59a575e586db4f4f2ee2f851d0825f94eeaca7ca
DJYMB <75224175+DJYMB@users.noreply.github.com>
2023-08-09 23:06:39 +0200

    app.js

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
@@ -56,11 +60,69 @@ btn4.addEventListener("click", function(){
 	}
 });
 
+btn5.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Option 1");
+		item = "5"; // Предположим, что "5" соответствует новой опции
+		tg.MainButton.show();
+	}
+});
+
+btn6.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Debitores!");
+		item = "4";
+		tg.MainButton.show();
+	}
+});
+
+btn7.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Debitores!");
+		item = "4";
+		tg.MainButton.show();
+	}
+});
+
+btn8.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Debitores!");
+		item = "4";
+		tg.MainButton.show();
+	}
+});
+
+
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
 
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	if (item === "1") {
+		// Логика для действия "Bank!"
+	}
+	else if (item === "2") {
+		// Логика для действия "My Catalog!"
+	}
+	// Аналогично для остальных действий
+	else if (item === "5") {
+		// Логика для действия "Option 1"
+	}
+	// Добавьте дополнительные условия для остальных действий
+});
 
 let usercard = document.getElementById("usercard");
 
